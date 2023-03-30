ea0e69c06671a209cd4bd052d564dac9ce2a80e5
andreasvesalius <veronvesaliuskereh@gmail.com>
2023-03-30 09:42:49 +0700

    commit cp3

--- src/main/kotlin/com/veron/kotlin/cp1/App2.kt
+++ src/main/kotlin/com/veron/kotlin/cp1/App2.kt
@@ -2,19 +2,38 @@ package com.veron.kotlin.cp1
 
 import java.io.File
 
-data class Answer(val number: Int, val answer: Char)
+import java.util.Scanner
 
 fun main() {
-    val inputFile = File("testdata.in")
-    val t = inputFile.readLine().toInt()
-    repeat(t) {
-        val n = inputFile.readLine().toInt()
-        val answers = Array(n) {
-            val (number, answer) = inputFile.readLine().split(". ")
-            Answer(number.toInt(), answer.first())
+    val scanner = Scanner(System.`in`)
+    val t = scanner.nextInt()
+
+    for (i in 1..t) {
+        val n = scanner.nextInt()
+        val matrix = Array(n) { IntArray(n) }
+
+        // read the matrix from input
+        for (row in 0 until n) {
+            for (col in 0 until n) {
+                matrix[row][col] = scanner.nextInt()
+            }
         }
-        answers.sortBy { it.number }
-        println("Test Case #${it + 1}:")
-        answers.forEach { println("${it.number}. ${it.answer}") }
+
+        // check if the matrix is symmetric
+        var isSymmetric = true
+        for (row in 0 until n) {
+            for (col in row + 1 until n) {
+                if (matrix[row][col] != matrix[col][row]) {
+                    isSymmetric = false
+                    break
+                }
+            }
+            if (!isSymmetric) {
+                break
+            }
+        }
+
+        // print the result
+        println("Test#$i:${if (isSymmetric) "Symmetric" else "Non-symmetric"}.")
     }
 }
